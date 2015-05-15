eb6180190a4d0390d1145a5cbb4dd5d210593997
jim syyap <jimsyyap@gmail.com>
2015-05-15 14:03:08 +1200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="author" content="Jim Syyap">
+    <meta name="description" content="Jim Syyap & Company, Digital Marketing Solutions">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <link rel='stylesheet' href='style.css' />
+    <!--[if lt IE 9]>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<h1>hola php</h1>
+<p><a href="http://www.convertmost.com">convertmost</a></p>
+
+</body>
+</html>
